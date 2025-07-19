1066bf64f99589a45c1609aa0ace72f5ec0cd956
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-07-20 00:25:50 +0300

    add folder App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { SearchBar } from "./components/SearchBar";
-import { fetchMovies } from "./services/movieService";
-import { Movie } from "./types/movie";
-import { MovieGrid } from "./components/MovieGrid/MovieGrid";
-import { MovieModal } from "./components/MovieModal/MovieModal";
-import { Loader } from "./components/Loader/Loader";
-import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import { SearchBar } from "../SearchBar";
+import { fetchMovies } from "../../services/movieService";
+import { Movie } from "../../types/movie";
+import { MovieGrid } from "../MovieGrid/MovieGrid";
+import { MovieModal } from "../MovieModal/MovieModal";
+import { Loader } from "../Loader/Loader";
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { Toaster } from "react-hot-toast";
 import toast from "react-hot-toast";
 
