3cc138c996859ff1f4ab47ba1e4be138385662ea
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-07-19 23:31:20 +0300

    created the project

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { SearchBar } from "./components/SearchBar";
+import { fetchMovies } from "./services/movieService";
+import { Movie } from "./types/movie";
+import { MovieGrid } from "./components/MovieGrid/MovieGrid";
+import { MovieModal } from "./components/MovieModal/MovieModal";
+import { Loader } from "./components/Loader/Loader";
+import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (query: string) => {
+    setLoading(true);
+    setError(false);
+    setMovies([]);
+
+    try {
+      const results = await fetchMovies(query);
+
+      if (results.length === 0) {
+        toast("No movies found for your request.");
+      }
+
+      setMovies(results);
+    } catch (error) {
+      console.error("Помилка під час пошуку фільмів:", error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+
+      {!loading && !error && movies.length > 0 && (
+        <MovieGrid
+          movies={movies}
+          onSelect={(movie) => setSelectedMovie(movie)}
+        />
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/services/movieService.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const options = {
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    Accept: "application/json",
+  },
+};
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get(`${BASE_URL}/search/movie`, {
+    ...options,
+    params: {
+      query,
+      include_adult: false,
+      language: "en-US",
+      page: 1,
+    },
+  });
+
+  return response.data.results;
+};
+console.log("TMDB TOKEN:", import.meta.env.VITE_TMDB_TOKEN);
